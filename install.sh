0b6327e5b59e279b2151e6d8229480d8335c60a8
shegx01 <shegx01@gmail.com>
2023-07-01 15:08:33 +0300

    rename tmux to .tmux

--- install.sh
+++ install.sh
@@ -161,5 +161,5 @@ csrutil status
 echo "Do not forget to disable SIP and reconfigure keyboard -> $HOME/.config/keyboard..."
 open "$HOME/.config/keyboard/KeyboardModifierKeySetup.png"
 echo "Add sudoer manually:\n '$(whoami) ALL = (root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | awk "{print \$1;}") $(which yabai) --load-sa' to '/private/etc/sudoers.d/yabai'"
-echo "Installation complete...\nRun nvim +PackerSync and Restart..."
+echo "Installation complete... Restart..."
 
